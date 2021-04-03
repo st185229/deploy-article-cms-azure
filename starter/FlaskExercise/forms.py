de15da05f0ab1b5ea40b91301bb772b05c9c34af
Suresh Thomas <st185229@ncr.com>
2021-04-03 19:12:08 +0100

    delete starter project

--- starter/FlaskExercise/forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileAllowed
-from wtforms import SubmitField
-
-class AnimalForm(FlaskForm):
-    image_path = FileField('Image', validators=[FileAllowed(['jpg', 'png'], 'Images only!')])
-    submit = SubmitField('Save')
